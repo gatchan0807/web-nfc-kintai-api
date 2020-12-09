06bda59856b81c4e805968fb7a8e7d1090094e65
がっちゃん <yuki.dolphin.koga@gmail.com>
2020-12-10 03:00:25 +0900

    ✨ 受け取ったと思われるparameterを返す処理を追加

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
-export function doGet(): void {
+export function doGet(
+  event: GoogleAppsScript.Events.DoGet
+): GoogleAppsScript.Content.TextOutput {
   Logger.log("HEY!");
+
+  return ContentService.createTextOutput(
+    JSON.stringify(event.parameter)
+  ).setMimeType(ContentService.MimeType.JSON);
 }
