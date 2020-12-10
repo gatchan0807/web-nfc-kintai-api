d3550d517ef4961a66fa7867d73adbb5dd7e9638
がっちゃん <yuki.dolphin.koga@gmail.com>
2020-12-11 08:54:40 +0900

    ✨ Payloadからデータをフォーマットする処理を追加

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 export function doGet(
   event: GoogleAppsScript.Events.DoGet
 ): GoogleAppsScript.Content.TextOutput {
-  Logger.log("HEY!");
+  const payload = event.parameter as Payload;
+  const userData = {
+    cardId: "",
+    passcode: "",
+  };
 
-  return ContentService.createTextOutput(
-    JSON.stringify(event.parameter)
-  ).setMimeType(ContentService.MimeType.JSON);
+  if (payload.items) {
+    const { items } = payload;
+    userData.cardId = items.cardId;
+    userData.passcode = items.passcode;
+  }
+
+  return ContentService.createTextOutput(JSON.stringify(userData)).setMimeType(
+    ContentService.MimeType.JSON
+  );
 }
+
+type Payload = {
+  items: {
+    cardId: string;
+    passcode: string;
+  };
+};
